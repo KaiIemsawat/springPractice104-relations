c0600ccd14668c8b9ecf03f01391bd467b0b82f8
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-09-14 17:51:53 -0400

    many2many setup

--- 05-jpa-many2many/src/main/java/com/springboot/modelrelation/entity/Course.java
+++ 05-jpa-many2many/src/main/java/com/springboot/modelrelation/entity/Course.java
@@ -27,6 +27,16 @@ public class Course {
     @JoinColumn(name = "course_id")
     private List<Review> reviews;
 
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(
+//            Carefully check these lines
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> students;
+
     /* -- Constructors --*/
     public Course(){}
     public Course(String title) {
@@ -63,12 +73,26 @@ public class Course {
         this.reviews = reviews;
     }
 
+    public List<Student> getStudents() {
+        return students;
+    }
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
     /* -- Convenience Method -- */
     public void addReview(Review theReview) {
         if(reviews == null) {
             reviews = new ArrayList<>();
         }
         reviews.add(theReview);
+    }
+
+    public void addStudent(Student theStudent) {
+        if(students == null) {
+            students = new ArrayList<>();
+        }
+        students.add(theStudent);
     }
 
     /* -- toString() --*/
@@ -79,6 +103,7 @@ public class Course {
                 ", title='" + title + '\'' +
                 ", instructor=" + instructor +
                 ", reviews=" + reviews +
+                ", students=" + students +
                 '}';
     }
 }
